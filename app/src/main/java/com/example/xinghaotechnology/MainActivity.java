6ece8e041d5070769b0b31ba9c821890f22eac16
enobyte <enoraden@gmail.com>
2019-12-21 11:50:33 +0700

    request sip

--- app/src/main/java/com/example/xinghaotechnology/MainActivity.java
+++ app/src/main/java/com/example/xinghaotechnology/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -92,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
                     startActivity(new Intent(MainActivity.this, ChatActivity.class));
+                    finish();
                 }
             }
             return null;
